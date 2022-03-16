2c5d1a0c047e90bfb135decd4303a87848d80359
Peter Nikolov <petar.nikolov@fourth.com>
2022-03-16 15:30:48 +0200

    initial project

---
+++ BusinessTripAdmin/BusinessTripAdmin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BusinessTripAdmin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
