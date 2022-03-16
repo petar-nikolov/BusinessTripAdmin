38751dc8e040fcfe984ad74e1fac94af16e43e91
Peter Nikolov <petar.nikolov@fourth.com>
2022-03-16 21:44:06 +0200

    added model binders

--- BusinessTripAdmin/Program.cs
+++ BusinessTripAdmin/Program.cs
@@ -1,4 +1,6 @@
+using BusinessTripAdmin.Infrastructure;
 using BusinessTripAdmin.Infrastructure.Data;
+using BusinessTripAdmin.ModelBinders;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,7 +14,13 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews()
+    .AddMvcOptions(options =>
+    {
+        options.ModelBinderProviders.Insert(0, new DecimalModelBinderProvider());
+        options.ModelBinderProviders.Insert(1, new DateTimeModelBinderProvider(FormatingConstant.NormalDateFormat));
+        options.ModelBinderProviders.Insert(2, new DoubleModelBinderProvider());
+    });
 
 var app = builder.Build();
 
