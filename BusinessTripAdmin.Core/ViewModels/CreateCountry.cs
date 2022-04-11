eec1551bc639ca1174ab0f1c5f84fd60e2b2f100
Peter Nikolov <petar.nikolov@fourth.com>
2022-04-12 00:35:18 +0300

    Create Country

---
+++ BusinessTripAdmin.Core/ViewModels/CreateCountry.cs
@@ -0,0 +1,33 @@
+﻿using BusinessTripAdmin.Infrastructure.Constants;
+using BusinessTripAdmin.Infrastructure.Data.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace BusinessTripAdmin.Core.ViewModels
+{
+    public class CreateCountry
+    {
+        [Required]
+        [StringLength(ValidationConstants.CountryNameMaxLength)]
+        public string CountryName { get; set; }
+
+        public CurrencyCode CurrencyCode { get; set; }
+
+        [Required]
+        [StringLength(ValidationConstants.CurrencyMaxLength)]
+        public string TripCurrency { get; set; }
+
+        [StringLength(ValidationConstants.CurrencyMaxLength)]
+        public string? LocalCurrency { get; set; }
+
+        [StringLength(ValidationConstants.DescriptionMaxLength)]
+        public string? Description { get; set; }
+
+        public decimal DailyAllowance { get; set; }
+
+        public decimal AccomodationAllowance { get; set; }
+
+        public DateTime ValidFrom { get; set; }
+
+        public DateTime? ValidTo { get; set; }
+    }
+}
