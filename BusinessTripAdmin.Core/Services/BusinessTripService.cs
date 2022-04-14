cf67d962ed62275d35fb4f4b7ed73fee14f35129
Peter Nikolov <petar.nikolov@fourth.com>
2022-04-14 17:25:04 +0300

    add countries in viewbag

--- BusinessTripAdmin.Core/Services/BusinessTripService.cs
+++ BusinessTripAdmin.Core/Services/BusinessTripService.cs
@@ -2,8 +2,6 @@
 using BusinessTripAdmin.Core.ViewModels;
 using BusinessTripAdmin.Infrastructure.Data.Abstraction;
 using BusinessTripAdmin.Infrastructure.Data.DbModels;
-using BusinessTripAdmin.Infrastructure.Data.Identity;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace BusinessTripAdmin.Core.Services
@@ -24,9 +22,8 @@ namespace BusinessTripAdmin.Core.Services
         public async Task<ICollection<BusinessTripViewModel>> GelAllTrips(string userId)
         {
             var userOrgId = await _userService.GetOrganizationByUserId(userId);
-            var organizationEmployees = await _employeeService.GetAllEmployeesByOrganizationId(userOrg);
+            var organizationEmployees = await _employeeService.GetAllEmployeesByOrganizationId(userOrgId);
             var employeeIds = organizationEmployees.Where(x => x.IsActive).Select(x => x.EmployeeId).ToList();
-            var currentOrganizationLocalCountry = _applicationDbRepository.
             var businessTrips = await _applicationDbRepository.GetAll<EmployeeBusinessTrip>().Where(x => employeeIds.Any(eid => eid == x.EmployeeId)).Select(bt => new BusinessTripViewModel
             {
                 Employee = $"{bt.Employee.FirstName} {bt.Employee.LastName}",
--- BusinessTripAdmin/Controllers/BusinessTripController.cs
+++ BusinessTripAdmin/Controllers/BusinessTripController.cs
@@ -6,18 +6,21 @@ namespace BusinessTripAdmin.Controllers
     public class BusinessTripController : BaseController
     {
         private readonly IBusinessTripService _businessTripService;
+        private readonly ICountryService _countryService;
 
-        public BusinessTripController(IBusinessTripService businessTripService)
+        public BusinessTripController(IBusinessTripService businessTripService, ICountryService countryService)
         {
             _businessTripService = businessTripService;
+            _countryService = countryService;
         }
 
         [Route("BusinessTrip/GetBusinessTrips/{userId}")]
-        public async Task<IActionResult> GetBusinessTrips([FromRoute]string userId)
+        public async Task<IActionResult> GetBusinessTrips([FromRoute] string userId)
         {
             var businessTrips = await _businessTripService.GelAllTrips(userId);
+            var countries = await _countryService.GetAllCountries();
+            ViewBag.Countries = countries;
             return View(businessTrips);
-
         }
     }
 }
