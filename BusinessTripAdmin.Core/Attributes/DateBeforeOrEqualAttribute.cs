1f0055ce89c872e6ca6d83f31c38875047101f6c
Peter Nikolov <petar.nikolov@fourth.com>
2022-04-16 14:47:09 +0300

    add custom attribute

---
+++ BusinessTripAdmin.Core/Attributes/DateBeforeOrEqualAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BusinessTripAdmin.Core.Attributes
+{
+    public class DateBeforeOrEqualAttribute : GeneralValidationAttribute
+    {
+        public DateBeforeOrEqualAttribute(string dateToCompareToFieldName)
+        {
+            DateToCompareToFieldName = dateToCompareToFieldName;
+        }
+        private string DateToCompareToFieldName { get; set; }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            DateTime earlierDate = (DateTime)value;
+
+            DateTime laterDate = (DateTime)validationContext.ObjectType.GetProperty(DateToCompareToFieldName).GetValue(validationContext.ObjectInstance, null);
+
+            if (laterDate < earlierDate)
+            {
+                return new ValidationResult("Date is not earlier");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
